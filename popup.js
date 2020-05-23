77af564410cf2f450c12e5ce00ecaf1cd20fac4e
afarhansib <afarhansib@gmail.com>
2020-05-24 02:55:41 +0700

    menambahkan fitur rekomendasi

--- popup.js
+++ popup.js
@@ -1,9 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', async function() {
+  const recommendation = await changeRecommendation()
   let formatItButton = document.getElementById('formatIt')
   formatItButton.addEventListener('click', function() {
     getTableData()
       .then(data => {
-        chrome.tabs.create({ url: chrome.runtime.getURL(`result.html?data=${encodeURIComponent(JSON.stringify(data))}`) })
+        chrome.tabs.create({ url: chrome.runtime.getURL(`result.html?recommendation=${recommendation}&data=${encodeURIComponent(JSON.stringify(data))}`) })
       })
   })
 })
@@ -28,4 +29,119 @@ const getData = () => {
     }
   })
   return data
+}
+
+const getDateToday = () => {
+  const months = [
+    "Januari",
+    "Februari",
+    "Maret",
+    "April",
+    "Mei",
+    "Juni",
+    "Juli",
+    "Agustus",
+    "September",
+    "Oktober",
+    "November",
+    "Desember"
+  ]
+
+  const days = [
+    "Minggu",
+    "Senin",
+    "Selasa",
+    "Rabu",
+    "Kamis",
+    "Jumat",
+    "Sabtu"
+  ]
+
+  let today = new Date();
+  // let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+  // let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  // let dateTime = date + ' ' + time;
+  return new Promise((resolve, reject) => {
+    resolve([`${days[today.getDay()]}`, `${today.getDate()} ${months[today.getMonth()]} ${today.getFullYear()}`])
+  })
+}
+
+const getRecommendation = date => {
+  const recommendationList = {
+    "8 Mei 2020": "2%",
+    "8 Mei 2020": "4%",
+    "8 Mei 2020": "6%",
+    "8 Mei 2020": "7%",
+    "9 Mei 2020": "9%",
+    "10 Mei 2020": "11%",
+    "11 Mei 2020": "12%",
+    "12 Mei 2020": "14%",
+    "13 Mei 2020": "16%",
+    "14 Mei 2020": "17%",
+    "15 Mei 2020": "19%",
+    "16 Mei 2020": "19%",
+    "17 Mei 2020": "21%",
+    "17 Mei 2020": "23%",
+    "18 Mei 2020": "24%",
+    "18 Mei 2020": "26%",
+    "19 Mei 2020": "28%",
+    "20 Mei 2020": "29%",
+    "21 Mei 2020": "31%",
+    "22 Mei 2020": "33%",
+    "23 Mei 2020": "34%",
+    "24 Mei 2020": "36%",
+    "24 Mei 2020": "38%",
+    "25 Mei 2020": "39%",
+    "26 Mei 2020": "41%",
+    "26 Mei 2020": "43%",
+    "27 Mei 2020": "45%",
+    "28 Mei 2020": "46%",
+    "28 Mei 2020": "48%",
+    "29 Mei 2020": "50%",
+    "30 Mei 2020": "51%",
+    "30 Mei 2020": "53%",
+    "31 Mei 2020": "55%",
+    "31 Mei 2020": "56%",
+    "1 Juni 2020": "58%",
+    "1 Juni 2020": "60%",
+    "1 Juni 2020": "62%",
+    "2 Juni 2020": "63%",
+    "2 Juni 2020": "65%",
+    "3 Juni 2020": "67%",
+    "3 Juni 2020": "68%",
+    "4 Juni 2020": "70%",
+    "5 Juni 2020": "72%",
+    "5 Juni 2020": "73%",
+    "6 Juni 2020": "75%",
+    "7 Juni 2020": "77%",
+    "7 Juni 2020": "78%",
+    "8 Juni 2020": "80%",
+    "8 Juni 2020": "82%",
+    "9 Juni 2020": "84%",
+    "10 Juni 2020": "84%",
+    "11 Juni 2020": "85%",
+    "11 Juni 2020": "87%",
+    "12 Juni 2020": "89%",
+    "12 Juni 2020": "90%",
+    "13 Juni 2020": "92%",
+    "13 Juni 2020": "94%",
+    "14 Juni 2020": "95%",
+    "15 Juni 2020": "97%",
+    "15 Juni 2020": "99%",
+    "16 Juni 2020": "100%",
+    "20 Juni 2020": "100%"
+  }
+
+  return recommendationList[date]
+}
+
+const changeRecommendation = async () => {
+  let recommendation = 0
+  await getDateToday()
+    .then(date => {
+      recommendation = Number(getRecommendation(date[1]).slice(0, -1))
+      document.querySelector('label span').textContent = `${date[0]}, ${date[1]}`
+      document.getElementById('rekomendasi').setAttribute("value", recommendation)
+    })
+  return recommendation
 }
--- result.js
+++ result.js
@@ -5,15 +5,25 @@ document.addEventListener('DOMContentLoaded', function() {
 
 const getDataFromURL = () => {
   return new Promise((resolve, reject) => {
-    let data = location.search.split('data=')[1]
-    resolve(JSON.parse(decodeURIComponent(data)))
+    let data = location.search.split('?')[1]
+    data = data.split('&')
+    recommendation = data[0].split('=')[1]
+    data = data[1].split('=')[1]
+    resolve([recommendation, JSON.parse(decodeURIComponent(data))])
   })
 }
 
 const compare = (a, b) => {
-  const sA = a[0]
-  const sB = b[0]
+  let sA = Number(a[0].slice(0, -1))
+  let sB = Number(b[0].slice(0, -1))
   let comparison = 0
+
+  if (isNaN(sA)) {
+    sA = 100
+  }
+  if (isNaN(sB)) {
+    sB = 100
+  }
 
   if (sA > sB) {
     comparison = 1
@@ -25,10 +35,58 @@ const compare = (a, b) => {
   return comparison * -1
 }
 
-const colorPercent = (percent, start = 0, end = 120) => {
-  var a = percent / 100,
-    b = (end - start) * a,
-    c = b + start;
+const addZero = num => {
+  return num > 9 ? num : `0${num}`
+}
+
+const getDateRightNow = () => {
+  const months = [
+    "Januari",
+    "Februari",
+    "Maret",
+    "April",
+    "Mei",
+    "Juni",
+    "Juli",
+    "Agustus",
+    "September",
+    "Oktober",
+    "November",
+    "Desember"
+  ]
+
+  const days = [
+    "Minggu",
+    "Senin",
+    "Selasa",
+    "Rabu",
+    "Kamis",
+    "Jumat",
+    "Sabtu"
+  ]
+
+  let today = new Date();
+  // let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+  let time = addZero(today.getHours()) + ":" + addZero(today.getMinutes()) + ":" + addZero(today.getSeconds());
+  // let dateTime = date + ' ' + time;
+  return `${days[today.getDay()]}, ${today.getDate()} ${months[today.getMonth()]} ${today.getFullYear()} ${time}`
+}
+
+const colorPercent = (percent, mid = 95, start = 0, end = 100) => {
+  // mengatur middle value
+
+  // RUWET !!!
+  // menghabiskan 3+ jam bagi saya untuk menemukan solusi ini
+  if (percent <= mid) {
+    percent = percent * (50 / mid)
+  } else {
+    percent -= mid
+    let divider = (100 - mid) / 50
+    percent = (percent / divider) + 50
+  }
+  let a = percent / 100
+  let b = (end - start) * a
+  let c = b + start
 
   // Return a CSS HSL string
   // diubah menjadi 2 nol di belakang koma
@@ -41,35 +99,45 @@ const writeTable = data => {
   // console.log(encodeURIComponent(data.toString()))
   // console.log(encodeURIComponent(data.toString()).length)
   let tableHTML = `
-		<table>
-			<caption contenteditable>Daftar Progress Peserta MPWA afarhansib</caption>
-			<thead>
-				<tr>
-					<th>No</th>
-					<th>Progress</th>
-					<th>Nama</th>
-				</tr>
-			</thead>
-			<tbody>`
-  data.sort(compare)
+    <table>
+      <caption contenteditable>Daftar Progress Peserta MPWA afarhansib</caption>
+      <thead>
+        <tr>
+          <th>No</th>
+          <th>Progress</th>
+          <th>Nama</th>
+        </tr>
+      </thead>
+      <tbody>`
+  data[1].sort(compare)
 
-  data.forEach((e, i) => {
+  data[1].forEach((e, i) => {
     let color = `white`
     if (e[0] !== "Lulus") {
-      color = colorPercent(Number(e[0].slice(0, -1)))
+      color = colorPercent(Number(e[0].slice(0, -1)), data[0])
     }
     tableHTML += `
-		    	<tr>
-					<td>${i + 1}</td>
-					<td style="background-color: ${color}">${e[0]}</td>
-					<td style="background-color: ${color}">${e[1]}</td>
-		    	</tr>`
+          <tr>
+          <td>${i + 1}</td>
+          <td style="background-color: ${color}">${e[0]}</td>
+          <td style="background-color: ${color}">${e[1]}</td>
+          </tr>`
   })
 
   tableHTML += `
-			</tbody>
-		</table>
-	`
+      </tbody>
+      <tfoot>
+        <tr>
+          <td colspan="2">Update</td>
+          <td>: ${getDateRightNow()}</td>
+        </tr>
+        <tr>
+          <td colspan="2">Rekomendasi</td>
+          <td>: ${data[0]}%</td>
+        </tr>
+      </tfoot>
+    </table>
+  `
 
   document.body.innerHTML = tableHTML
 }
